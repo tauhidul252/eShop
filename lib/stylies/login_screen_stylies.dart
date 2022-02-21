c08397bcdc3582be0838bf80b6fa24d6b355c447
Tauhidul <tauhidulislam252@gmail.com>
2022-02-21 07:31:13 +0600

    Login screen Login screen 2 buttons and text set reset is set

---
+++ lib/stylies/login_screen_stylies.dart
@@ -0,0 +1,9 @@
+import 'package:eshop/appColors/app_colors.dart';
+import 'package:flutter/cupertino.dart';
+
+class LoginScreenStylies{
+  static const TextStyle resetPasswordStylies=TextStyle(
+    color: AppColors.baseBlackColor,
+    fontSize: 18,
+  );
+}
